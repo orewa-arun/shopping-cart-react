6a90d0cc27a6d327ae4bd53823d78cc907d6ec58
orewa-arun <arunworkacc0@gmail.com>
2023-07-10 18:28:58 +0530

    added store rows and columns

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,14 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+
 export function Store() {
-    return <h1>Store</h1>;
+    return <>
+        <h1>Store</h1>
+        {/* large means 3,medium means 2 column elements with gap 3 */}
+        <Row md={2} xs={1} lg={3} className="g-3">
+            {storeItems.map((item) => (
+                <Col>{JSON.stringify(item)}</Col>
+            ))}
+        </Row>
+    </>;
 }
