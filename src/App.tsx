f02444c47bceccebfe42bc4e61b6c812c63edc20
orewa-arun <arunworkacc0@gmail.com>
2023-07-10 11:41:29 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home.jsx';
+import { About } from './pages/About.js';
+import { Store } from './pages/Store.js';
 
 function App() {
-  return <h1>Hello!</h1>
+  return (
+    <Container>
+      <Routes>
+        {/* element and path are called props and they are like args to the component */}
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App
