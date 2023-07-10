9361cc09a25666c64d620aca31328ce232ae95a4
orewa-arun <arunworkacc0@gmail.com>
2023-07-10 12:30:59 +0530

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { Container } from 'react-bootstrap'
 import { Home } from './pages/Home.jsx';
 import { About } from './pages/About.js';
 import { Store } from './pages/Store.js';
+import { Navbar } from './components/Navbar.js';
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        {/* element and path are called props and they are like args to the component */}
-        <Route path='/' element={<Home />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          {/* element and path are called props and they are like args to the component */}
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+export function Navbar() {
+    return <NavbarBs>
+        {/* classes would be having css files in bootstrap */}
+        <Container className='bg-white shadow-sm mb-3 mt-1'>
+            <Nav>
+                <Nav.Link to="/" as={NavLink}>
+                    Home
+                </Nav.Link>
+                <Nav.Link to="/store" as={NavLink}>
+                    Store
+                </Nav.Link>
+                <Nav.Link to="/about" as={NavLink}>
+                    About
+                </Nav.Link>
+            </Nav>
+        </Container>
+    </NavbarBs>
+}
